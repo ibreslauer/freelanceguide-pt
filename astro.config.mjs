ce15f4c5044775c33057411e6a72f1ec4b457595
Ivan Breslauer <ivan.breslauer@gmail.com>
2025-11-22 21:53:27 +0000

    Fix layout; fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ibreslauer.github.io/freelanceguide-pt/',
-  base: '/freelanceguide-pt',
+  site: "https://ibreslauer.github.io/freelanceguide-pt/",
+  base:
+    process.env.NODE_ENV === "production" ? "/freelanceguide-pt" : undefined,
   integrations: [mdx(), sitemap()],
 
   vite: {
